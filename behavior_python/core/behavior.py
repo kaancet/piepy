66a4d1ab7f79bb0f5a5d401ab1c29e4fc12e8464
Kaan Cetindag <cetindag.kaan@gmail.com>
2023-08-01 16:55:54 +0200

    WIP: Implementing polars in behavior

--- behavior_python/core/behavior.py
+++ behavior_python/core/behavior.py
@@ -1,32 +1,29 @@
 from .core import *
 import glob
 import natsort
-        
+import scipy.io as sio
+
 
 class BehaviorData:
     """ Keeps the data as a huge csv of appended session data"""
-    __slots__ = ['summary_data','cumul_data','_convert','dateinterval']
-    def __init__(self,dateinterval:str=None) -> None:
+    __slots__ = ['summary_data','cumul_data']
+    def __init__(self) -> None:
         self.summary_data = None
         self.cumul_data = None
-        self.dateinterval = dateinterval
         
-    def filter_dates(self):
+    def filter_dates(self,dateinterval:list):
         """Filters the behavior data to analyze only the data in the date range"""
-        if self.dateinterval is None:
-            self.dateinterval = [self.summary_data['date'].iloc[0],self.summary_data['date'].iloc[-1]]
-            
-        # dateinterval is a list of two date strings e.g. ['200127','200131']
-        if type(self.dateinterval) is not list:
-            self.dateinterval = [self.dateinterval]
-            # add current day as end date
-            self.dateinterval.append(dt.today().strftime('%y%m%d'))
 
-        if len(self.dateinterval) == 2:
-            startdate = dt.strptime(self.dateinterval[0], '%y%m%d')
-            enddate = dt.strptime(self.dateinterval[1], '%y%m%d')
-        else: 
-            raise Exception('Date interval wrong!')
+        # dateinterval is a list of two date strings e.g. ['200127','200131']
+        if isinstance(dateinterval,str):
+            dateinterval = [dateinterval]
+            # add current day as end date
+            dateinterval.append(dt.today().strftime('%y%m%d'))
+        else:
+            assert len(dateinterval)<=2, f'You need to provide a single start(1) or start and end dates(2), got {len(dateinterval)} dates'
+
+        startdate = dt.strptime(dateinterval[0], '%y%m%d')
+        enddate = dt.strptime(dateinterval[1], '%y%m%d')
 
         display('Retreiving between {0} - {1}'.format(startdate,enddate))
 
@@ -38,66 +35,35 @@ class BehaviorData:
         
     def save(self,path:str,task_type:str)->None:
         """ Saves the data in the given location"""
-        cumul_save_name = pjoin(path,f'{task_type}TrainingData.behave').replace("\\","/")
+        cumul_save_name = pjoin(path,f'{task_type}TrainingData.parquet').replace("\\","/")
         summary_save_name = pjoin(path,f'{task_type}TrainingDataSummary.csv').replace("\\","/")
-        self.summary_data.to_csv(summary_save_name,index=False)
-        self.cumul_data.to_pickle(cumul_save_name)
         
-    def make_saveable(self) -> pd.DataFrame:
-        """ The columns that have numpy.ndarrays need to be saved as lists in DataFrame columns!
-        A copy of the session_data is created with converted columns to be saved"""        
-        save_df = self.cumul_data.copy(deep=True)
-        for col in self._convert:
-            save_df[col] = save_df[col].apply(lambda x: x.tolist())
-        return save_df
-
-    def make_loadable(self) -> None:
-        """ Converts the necessary columns into numpy array """
-        for col in self._convert:
-            if not isinstance(self.cumul_data[col].iloc[0], np.ndarray):
-                self.cumul_data[col] = self.cumul_data[col].apply(eval).apply(lambda x: np.array(x))
-    
-
-class BehaviorStats:
-    __slots__ = ['handling_days','habituation_days','training_days',
-                 'experiment_days','trial_count','session_count']
-    def __init__(self,data:BehaviorData) -> None:
-        self.init_from_data(data)
+        # cast sf and tf to str
+        summary_save_data = self.summary_data.with_columns([("[" + pl.col("sf").cast(pl.List(pl.Utf8)).arr.join(", ") + "]").alias('sf'),
+                                                            ("[" + pl.col("tf").cast(pl.List(pl.Utf8)).arr.join(", ") + "]").alias('tf')])
         
-    def init_from_data(self,data:BehaviorData) -> None:
-        summary_data = data.summary_data
-        cumul_data = data.cumul_data
-        
-        self.session_count = summary_data['session_no'].iloc[-1]
-        self.handling_days = len(summary_data[summary_data['paradigm']=='handling'])
-        self.habituation_days = len(summary_data[summary_data['paradigm']=='habituation'])
-        self.training_days = len(summary_data[summary_data['paradigm'].str.contains('training')])
-        self.experiment_days = len(summary_data[summary_data['paradigm'].str.contains('experiment')])
-        
-        self.tral_count = cumul_data['cumul_trial_no'].iloc
+        summary_save_data.write_csv(summary_save_name)
+        self.cumul_data.write_parquet(cumul_save_name)
 
 
 class Behavior:
     """ Analyzes the training progression of animals through multiple sessions
-            animalid:     id of the animal to be analyzed(e.g. KC033)
-            dateinterval: interval of dates to analyze the data, 
-                          can be a list of two dates, or a string of starting date
-                          (e.g. ['200110','200619'] OR '200110')
-            load_behave:  flag to either load previously analyzed behavior data or to analyze it from scratch
-            load_data:    flag to either load previously parsed data or to parse it again
-            """
-    def __init__(self,animalid,dateinterval=None,load_data:bool=True,load_behave:bool=True,*args,**kwargs) -> None:
+        animalid:     id of the animal to be analyzed(e.g. KC033)
+        load_type:    string to set how to load
+                      'lazy' = loads only the last analyzed data, doesn't analyze and add new sessions since last analysis
+                      'full' = loads all the data and adds new sessions to the loaded data
+                      'sessions' = loads the session data and reanalyzes the behavior data from that
+                      'noload' = doesn't load anything reanalyzes the sessions data from scratch
+    """
+    def __init__(self,
+                 animalid:str,
+                 load_type:str='full', # ['lazy','full','sessions','noload']
+                 *args,**kwargs) -> None:
         self.animalid = animalid
-        self.dateinterval = dateinterval
-        self.load_behave = load_behave
-        self.load_data = load_data
-
-        # no point in reading saved behavior data if reanalyzing session data
-        if not self.load_data:
-            self.load_behave = False
+        self.load_type = load_type
 
         self.init_data_paths()
-        self.get_sessions()
+        self.session_list = self.get_sessions()
         
         self.db_interface = DataBaseInterface(self.databasepath)
         self.read_googlesheet()
@@ -110,24 +76,36 @@ class Behavior:
         self.analysisfolder = config['analysisPath']
         self.databasepath = config['databasePath']
         
-    def get_sessions(self) -> None:
-        """ self.session_list   n x 2 matrix that has the sesion directory names in the 1st column
-                                and session dates in datetime format in the 2nd column"""
-        experiment_sessions = glob.glob('{0}/*{1}*__no_cam_*/'.format(self.presentationfolder,self.animalid))
-        training_sessions = glob.glob('{0}/*{1}*__no_cam_*/'.format(self.trainingfolder,self.animalid))
-        onep_sessions = glob.glob('{0}/*{1}*__1P_*/'.format(self.trainingfolder,self.animalid))
-        twop_sessoins = glob.glob('{0}/*{1}*__2P_*/'.format(self.trainingfolder,self.animalid))
+    def get_sessions(self) -> list:
+        """ self.session_list   n x 3 matrix that has the sesion directory names in the 1st column
+                                and session dates in datetime format in the 2nd column and
+                                session type in the 3rd column"""
         
-        s = experiment_sessions + training_sessions + onep_sessions
-        session_list = [x.split(os.sep)[-2] for x in s]
-        session_list = natsort.natsorted(session_list, reverse=False)
+        experiment_sessions = glob.glob('{0}/*{1}*__no_cam_*/'.format(self.presentationfolder,self.animalid))
+        onep_sessions = glob.glob('{0}/*{1}*__1P_*/'.format(self.presentationfolder,self.animalid))
+        twop_sessions = glob.glob('{0}/*{1}*__2P_*/'.format(self.presentationfolder,self.animalid))
+        training_sessions = glob.glob('{0}/*{1}*__no_cam_*/'.format(self.trainingfolder,self.animalid))
+        
+        s = experiment_sessions + training_sessions + onep_sessions + twop_sessions
+        tmp = [x.split(os.sep)[-2] for x in s]
+        tmp_ordered = natsort.natsorted(tmp, reverse=False)
         
         # failsafe for duplicates due to data existing both in training and presentation
-        session_list = np.unique(np.array(session_list)).tolist()
+        tmp_ordered = np.unique(np.array(tmp_ordered)).tolist()
+        
+        session_list = []
+        for sesh in tmp_ordered:
+            date = dt.strptime(sesh.split('_')[0], '%y%m%d')
+            if 'opto' in sesh:
+                session_list.append([sesh,'opto',date])
+            elif '1P' in sesh:
+                session_list.append([sesh,'1P',date])
+            elif '2P' in sesh:
+                session_list.append([sesh,'2P',date])
+            else:
+                session_list.append([sesh,'train',date])
 
-        date_list = [dt.strptime(x.split('_')[0], '%y%m%d') for x in session_list]
-
-        self.session_list = [[session_list[i], date] for i, date in enumerate(date_list)]
+        return session_list
     
     def read_googlesheet(self) -> None:
         """ Reads all the entries from the googlesheet with the current animal id"""
@@ -144,27 +122,27 @@ class Behavior:
         self.gsheet_df['Date [YYMMDD]'] = self.gsheet_df['Date [YYMMDD]'].apply(lambda x: str(x))
         self.gsheet_df['Date_dt'] = pd.to_datetime(self.gsheet_df['Date [YYMMDD]'], format='%y%m%d')
         
-    
     def get_googlesheet_data(self,date:str,cols:list=None) -> pd.DataFrame:
         sheet_stats = {}
         if cols is None:
             cols = ['weight [g]','supp water [µl]','user','time [hh:mm]']
         #current date data
         row = self.gsheet_df[self.gsheet_df['Date [YYMMDD]']==date]
-        if len(row):
-            for c in cols:
-                key = c.split('[')[0].strip(' ') # get rid of units in column names
+        for c in cols:
+            key = c.split('[')[0].strip(' ') # get rid of units in column names
+            if len(row):
                 sheet_stats[key] = row[c].values[0]
-        else:
-            display('No Training Log entry {0} {1}'.format(self.animalid, date))
-            sheet_stats = {}
-
+            else:
+                sheet_stats[key] = None
+            
         return sheet_stats
     
     def get_non_data(self):
         """ Gets the non-data file rows from the googlesheet"""
         non_data = pd.DataFrame()
         temp = self.gsheet_df[self.gsheet_df['paradigm'].isin(['handling','water restriction start','surgery'])]
+    
+        non_data = pl.DataFrame({'weight'})
         non_data['date'] = temp['Date [YYMMDD]']
         non_data['weight'] = temp['weight [g]']
         non_data['paradigm'] = temp['paradigm']
@@ -180,20 +158,22 @@ class Behavior:
         
         self.behavior_data.save(self.savepath)
         display(f'Behavior data saved in {self.savepath}')
-        if not self.behavior_data.summary_data.empty and not self.behavior_data.cumul_data.empty:
+        
+        # deleting the old data
+        if not self.behavior_data.summary_data.is_empty() and not self.behavior_data.cumul_data.is_empty():
             if self.cumul_file_loc is not None:
                 if self.cumul_file_loc != latest_session:
                     display(f'Deleting the old data in {self.cumul_file_loc}')
-                    os.remove(pjoin(self.analysisfolder,self.cumul_file_loc,f'{task_type}TrainingData.behave'))
+                    os.remove(pjoin(self.analysisfolder,self.cumul_file_loc,f'{task_type}TrainingData.parquet'))
                     os.remove(pjoin(self.analysisfolder,self.summary_file_loc,f'{task_type}TrainingDataSummary.csv'))
 
     def isSaved(self,task_type:str) -> bool:
         """ Finds the session folder that has the saved behavior data"""
-        cumul_data_saved_loc = glob.glob(f'{self.analysisfolder}/*{self.animalid}*/{task_type}TrainingData.behave')
+        cumul_data_saved_loc = glob.glob(f'{self.analysisfolder}/*{self.animalid}*/{task_type}TrainingData.parquet')
         summary_data_saved_loc = glob.glob(f'{self.analysisfolder}/*{self.animalid}*/{task_type}TrainingDataSummary.csv')
         
         if len(cumul_data_saved_loc) > 1 and len(summary_data_saved_loc) > 1:
-            display(f'There should be only single _trainingData.behave (most recent one) found {summary_data_saved_loc}')
+            display(f'There should be only single _trainingData.parquet (most recent one) found {summary_data_saved_loc}')
             cumul_data_saved_loc = cumul_data_saved_loc[:1]
             summary_data_saved_loc = summary_data_saved_loc[:1]
         
@@ -215,7 +195,7 @@ class Behavior:
     def get_unanalyzed_sessions(self,task_type:str) -> list:
         """ Returns the list of sessions that have not been added to the behavior analysis"""
         is_saved = self.isSaved(task_type)
-        if self.load_behave:
+        if self.load_type == 'full':
             if is_saved:
                 reverse_session_list = self.session_list[::-1] #this will have sessions listed from new to old for ease of search
                 display(f'Found behavior data at {self.cumul_file_loc}')
@@ -226,7 +206,12 @@ class Behavior:
             else:
                 display('No behavior data present, creating new')
                 missing_sessions = self.session_list
-        else:
+                
+        elif self.load_type == 'sessions' or self.load_type == 'noload':
             missing_sessions = self.session_list
+        elif self.load_type == 'lazy':
+            # returns an empty list, no new session will be analyzed
+            missing_sessions = []
+            
         
         return missing_sessions
--- behavior_python/detection/wheelDetectionBehavior.py
+++ behavior_python/detection/wheelDetectionBehavior.py
@@ -1,15 +1,14 @@
 from .wheelDetectionSession import *
-from behavior_python.core.behavior import Behavior,BehaviorData,BehaviorStats
+from behavior_python.core.behavior import Behavior,BehaviorData
 
 
 class WheelDetectionBehaviorData(BehaviorData):
-    def __init__(self,dateinterval:str=None) -> None:
-        super().__init__(dateinterval)
+    def __init__(self) -> None:
+        super().__init__()
         self._convert = ['wheel','lick','reward']
         
     def __repr__(self):
-        rep = f''' Wheel Detection Behavior Data
-        date interval = {self.dateinterval} '''
+        rep = f''' Wheel Detection Behavior Data'''
         return rep
     
     def save(self,path:str)->None:
@@ -18,101 +17,140 @@ class WheelDetectionBehaviorData(BehaviorData):
 
 
 class WheelDetectionBehavior(Behavior):
-    def __init__(self, animalid:str, dateinterval:str=None,*args,**kwargs):
-        super().__init__(animalid,dateinterval,*args,**kwargs)
-        self.behavior_data = WheelDetectionBehaviorData(dateinterval)
+    def __init__(self, 
+                 animalid:str,
+                 load_type:str=None,
+                 *args,**kwargs):
+        super().__init__(animalid,load_type,*args,**kwargs)
+        
+        self.behavior_data = WheelDetectionBehaviorData()
         # get only detection
-        self.session_list = self.filter_sessions()
-        self.get_behavior(kwargs.get('just_load',False))
+        self.session_list = self.filter_sessions() # this is a failsafe to get only detect sessions
+        
+        self.get_behavior()
+        
         # here is where task specific things could go
-        self.behavior_data.filter_dates()
         self.save_behavior()
         
     def filter_sessions(self):
         return [i for i in self.session_list if 'detect' in i[0]]
 
     @timeit('Getting behavior data...')
-    def get_behavior(self,just_load:bool=False):
+    def get_behavior(self):
         """ Loads the behavior data(cumul and summary)"""
         missing_sessions = self.get_unanalyzed_sessions('detect')
         pbar = tqdm(missing_sessions)
         
         if len(missing_sessions) == len(self.session_list):
-            # no behavior csv, start from scratch
-            cumul_data = pd.DataFrame()
-            summary_data = pd.DataFrame()
+            # noload and sessions will enter here for sure
+            # full will enter here if only there is no data to load
+            cumul_data = pl.DataFrame()
+            summary_data = pl.DataFrame()
             session_counter = 0
         else:
             # this loads the most recent found data
-            cumul_data = pd.read_pickle(pjoin(self.analysisfolder,self.cumul_file_loc,'detectTrainingData.behave'))
-            summary_data = pd.read_csv(pjoin(self.analysisfolder,self.summary_file_loc,'detectTrainingDataSummary.csv'),dtype={'date':str})
-            # to_csv reads lists as string, parse them to actiual lists
-            summary_data.sf = summary_data.sf.apply(lambda x: [float(i) for i in x.strip("[]").split(", ")] if not isinstance(x,float) else x)
-            summary_data.tf = summary_data.tf.apply(lambda x: [float(i) for i in x.strip("[]").split(", ")] if not isinstance(x,float) else x)      
-
-            session_counter = summary_data['session_no'].iloc[-1]
+            # full and lazy will enter here
+            cumul_data = pl.read_parquet(pjoin(self.analysisfolder,self.cumul_file_loc,'detectTrainingData.parquet'))
+            summary_data = pl.read_csv(pjoin(self.analysisfolder,self.summary_file_loc,'detectTrainingDataSummary.csv'))
             
-        if not just_load:
-            summary_to_append = []
-            for i,sesh in enumerate(pbar):
-                pbar.set_description(f'Analyzing {sesh[0]} [{i+1}/{len(missing_sessions)}]')
-                detect_session = WheelDetectionSession(sesh[0],load_flag=self.load_data)
-                session_data = detect_session.data.data
-                
-                gsheet_dict = self.get_googlesheet_data(detect_session.meta.baredate,
-                                                        cols=['paradigm','supp water [µl]','user','time [hh:mm]','rig water [µl]'])
-                
-                if len(session_data):
-                    # add behavior related fields as a dictiionary
-                    summary_temp = {}
-                    summary_temp['date'] = detect_session.meta.baredate
-                    summary_temp['blank_time'] = detect_session.meta.openStimDuration
-                    summary_temp['response_window'] = detect_session.meta.closedStimDuration
-                    try:
-                        summary_temp['level'] = int(detect_session.meta.level)
-                    except:
-                        summary_temp['level'] = -1
-                    summary_temp['session_no'] = session_counter + 1
-                    
-                    # put data from session stats
-                    for k in detect_session.stats.__slots__:
-                        summary_temp[k] = getattr(detect_session.stats,k,None)
+            # sf and tf needs to be converted back to lists
+            summary_data = summary_data.with_columns([pl.col("sf")
+                                                     .str.replace_all("[",'',literal=True)
+                                                     .str.replace_all(']','',literal=True)
+                                                     .str.split(',')
+                                                     .apply(lambda x: [float(i) for i in x])
+                                                     .alias('sf'),
+                                                     pl.col("tf")
+                                                     .str.replace_all("[",'',literal=True)
+                                                     .str.replace_all(']','',literal=True)
+                                                     .str.split(',')
+                                                     .apply(lambda x: [float(i) for i in x])
+                                                     .alias('tf')
+                                                   ])
+            
+            session_counter = summary_data[-1,'session_no']
+        
+        summary_to_append = []
+        for i,sesh in enumerate(pbar):
+            # lazy will not enter here as missing sessions will be []
+            pbar.set_description(f'Analyzing {sesh[0]} [{i+1}/{len(missing_sessions)}]')
+            
+            if self.load_type == 'noload':
+                detect_session = WheelDetectionSession(sesh[0],load_flag=False)
+            else:
+                # 'full' or 'sessions' load type should enter here
+                detect_session = WheelDetectionSession(sesh[0],load_flag=True)
 
-                    # put values from session meta data
-                    summary_temp['weight'] = detect_session.meta.weight
-                    summary_temp['task'] = detect_session.meta.controller
-                    summary_temp['sf'] = detect_session.meta.sf_values
-                    summary_temp['tf'] = detect_session.meta.tf_values
-                    summary_temp['rig'] = detect_session.meta.rig
-                    summary_temp = {**summary_temp, **gsheet_dict}
-                    summary_to_append.append(summary_temp)
-                    
-                    # cumulative data
-                    session_data['session_no'] =  session_counter + 1
-                    session_data['date'] = detect_session.meta.baredate
-                    session_data['paradigm'] = gsheet_dict.get('paradigm','training_detection')
-                    
-                    cumul_data = pd.concat([cumul_data,session_data],ignore_index=True)
-                    cumul_data['cumul_trial_no'] = np.arange(len(cumul_data)) + 1
-                    session_counter += 1
-                else:
-                    display(f' >>> WARNING << NO DATA FOR SESSION {sesh[0]}')
-                    continue
+            session_data = detect_session.data.data
+            gsheet_dict = self.get_googlesheet_data(detect_session.meta.baredate,
+                                                    cols=['paradigm','supp water [µl]','user','time [hh:mm]','rig water [µl]'])
+            
+            if len(session_data):
+                # add behavior related fields as a dictiionary
+                summary_temp = {}
+                summary_temp['date'] = detect_session.meta.baredate
+                summary_temp['blank_time'] = detect_session.meta.openStimDuration
+                summary_temp['response_window'] = detect_session.meta.closedStimDuration
+                try:
+                    summary_temp['level'] = int(detect_session.meta.level)
+                except:
+                    summary_temp['level'] = -1
+                summary_temp['session_no'] = session_counter + 1
                 
-            if len(missing_sessions):
-                cumul_data = get_running_stats(cumul_data,window_size=50)
-                summary_to_append = pd.DataFrame(summary_to_append)
-                summary_data = pd.concat([summary_data,summary_to_append],ignore_index=True)
-                # adding the non-data stages of training once in the beginning
-                if len(missing_sessions) == len(self.session_list):
-                    non_data = self.get_non_data()
-                    summary_data = pd.concat([summary_data,non_data],ignore_index=True)
-                # Failsafe date sorting for non-analyzed all trials and empty sessions(?)
-                summary_data = summary_data.sort_values('date', ascending=True)
-                summary_data.reset_index(inplace=True,drop=True)
-  
-        # turn date column to str
-        summary_data['date']= summary_data['date'].apply(str)
+                # put data from session stats
+                for k in detect_session.stats.__slots__:
+                    summary_temp[k] = getattr(detect_session.stats,k,None)
+
+                # put values from session meta data
+                summary_temp['weight'] = detect_session.meta.weight
+                summary_temp['task'] = detect_session.meta.controller
+                summary_temp['sf'] = detect_session.meta.sf_values
+                summary_temp['tf'] = detect_session.meta.tf_values
+                summary_temp['rig'] = detect_session.meta.rig
+                summary_temp = {**summary_temp, **gsheet_dict}
+                
+                # cumulative data
+                session_data = session_data.with_columns([(pl.lit(session_counter+1)).alias('session_no'),
+                                                            (pl.lit(sesh[1])).alias('session_type')])
+                
+                if i == 0:
+                    summary_to_append = {k:[v] for k,v in summary_temp.items()}
+                    cumul_data = session_data
+                else:
+                    for k,v in summary_temp.items():
+                        summary_to_append[k].append(v)
+                    #sorting the columns
+                    session_data = session_data.select(cumul_data.columns)
+                    try:
+                        cumul_data = pl.concat([cumul_data,session_data])
+                    except:
+                        session_data = session_data.with_columns(pl.col('opto').cast(pl.Int64).alias('opto'))
+                        cumul_data = cumul_data.with_columns(pl.col('opto_region').cast(str).alias('opto_region'))
+                        session_data = session_data.with_columns(pl.col('opto_region').cast(str).alias('opto_region'))
+                        cumul_data = pl.concat([cumul_data,session_data])
+                
+                session_counter += 1 
+            else:
+                display(f' >>> WARNING << NO DATA FOR SESSION {sesh[0]}')
+                continue
+        
+        if 'cumul_trial_no' in cumul_data.columns:
+            cumul_data = cumul_data.drop('cumul_trial_no')
+            
+        cumul_trial_no = pl.Series('cumul_trial_no',np.arange(len(cumul_data)) + 1)
+        cumul_data = cumul_data.hstack([cumul_trial_no])
+        # cumul_data = get_running_stats(cumul_data,window_size=50)
+        if len(summary_to_append):
+            summary_data = pl.concat([summary_data,pl.DataFrame(summary_to_append)])
+               
+        # adding the non-data stages of training once in the beginning
+        # this should happen when loading non-lazy and 
+        # if len(missing_sessions) == len(self.session_list):
+        #     non_data = self.get_non_data()
+        #     summary_data = pl.concat([summary_data,non_data])
+        # Failsafe date sorting for non-analyzed all trials and empty sessions(?)
+            # summary_data = summary_data.sort('date')
+
         self.behavior_data.summary_data = summary_data
         self.behavior_data.cumul_data = cumul_data
             
