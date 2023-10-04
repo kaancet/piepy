024c434403433aa44a5003b59c282f4fedaa6a31
DMJoseph <32844940+DMJoseph@users.noreply.github.com>
2023-10-04 13:40:55 +0200

    Preprocessing table updates
    Processing through till neuropil correction and df/f calculation more robust.

--- datajoint_pipeline/tables/main_tables.py
+++ datajoint_pipeline/tables/main_tables.py
@@ -4,7 +4,7 @@ import numpy as np
 # from suite2p.gui import gui2p
 import os
 import subprocess
-from intermediaries.run_fissa_correction import run_fissa_correction
+from datajoint_pipeline.intermediaries.run_fissa_correction import run_fissa_correction
 from natsort import natsorted
 from behavior_python.detection.wheelDetectionSession import WheelDetectionSession
 import matplotlib.pyplot as plt
@@ -76,6 +76,7 @@ class Recording(dj.Manual):
     rec_idx     : int           # index of the recordings on the day
     ---
     depth       : float         # Depth of the recording in um
+    plane_step  : float         # The step in distance between planes - may extract it directly from the file header later
     wavelength  : float         # Wavelength of the recording in nm
     laser_power : float         # Power in mW
     recording_name : varchar(250) # Later it can be automatic, based on other fields
@@ -117,99 +118,3 @@ class Behaviour(dj.Imported):
 
         key['n_trials'] = behave_df.shape[0]
         self.insert1(key)
-
-
-# # TODO restructure to pull table from Preprocessing - Remove once outdated
-# @schema
-# class RawTwoData(dj.Imported):
-#     definition = """
-#     # The output from suite2p registration and ROI extraction
-#     -> Recording
-#     ---
-#     reg_data_path         : varchar(500)  # Path to the registered data - probably important in case it's changed. E.g. cell selection    
-#     """
-
-#     class Plane(dj.Part):
-#         definition = """
-#         # The neuropil corrected dF/F for each plane of the recording
-#         -> RawTwoData
-#         plane_n                 : int           # The number of the plane from the recording
-#         ---
-#         cell_select_done        : int           # 0 if not yet complete, 1 otherwise
-#         plane_path              : varchar(500)  # The path to the registered data for that specific plane
-#         data_df                 : longblob      # The dF/F trace that is output from FISSA
-#         """
-#         # TODO Maybe remove the cell select done field and find a way to make cell selection optional
-
-#     # TODO add sanity check that the number of planes in the suite2p folder matches the number of planes marked for the recording
-#     def make(self, key):
-#         core_reg_path = r'\\nerffs17\boninlabwip2023\data\2photon\reg'
-#         reg_data_path = os.path.join(core_reg_path, (Recording & key).fetch1('recording_name'), 'suite2p')
-#         self.insert1({**key, 'reg_data_path': reg_data_path})
-
-#         # Run through each plane for multi-plane recordings
-#         plane_dirs = [f.name for f in os.scandir(reg_data_path) if f.is_dir() and 'plane' in f.name]
-#         ordered_planes = natsorted(plane_dirs)
-#         for p in ordered_planes:
-#             p_num = int(p[5:])
-
-#             sg.theme('DarkAmber')
-#             layout = [[sg.Text('Run cell selection?')],
-#                       [sg.Button('Ok'), sg.Button('No')] ]
-#             window = sg.Window((Recording & key).fetch1('recording_name'), layout)
-
-#             while True:
-#                 # TODO make this window more informative - session, plane num, etc
-#                 event, values = window.read()
-#                 if event == sg.WIN_CLOSED or event == 'No':
-#                     break
-#                 elif event == 'Ok':
-#                     window.close()
-#                     plane_path = os.path.join(reg_data_path, 'plane0')
-
-#                     stat_path = os.path.join(plane_path, 'stat.npy')
-#                     result = subprocess.run(["python", "run_suite2p_gui.py", "-i", stat_path], capture_output=True)
-#                     print(result)
-#                     print('Cell selection done')
-
-#                     sampling_freq = 30
-#                     """fissa_out = run_fissa_correction(plane_path, sampling_freq)
-#                      #print(fissa_out)
-#                     print('FISSA finished')
-
-#                     df_data = fissa_out.deltaf_result
-#                     # Pull the dF/F trace out of the cell
-#                     num_frames = np.sum([np.shape(df_data[0, frames])[1] for frames in range(df_data.shape[1])])
-#                     num_neurons = df_data.shape[0]
-#                     df_cat = np.empty((num_neurons, num_frames))
-#                     for nn in num_neurons:
-#                         nn_trace = np.hstack([df_data[0, frames][0,:] for frames in range(df_data.shape(1))])
-#                         df_cat[nn, :] = nn_trace
-
-#                     print('df successfully extracted')
-
-#                     # Quick test plot
-#                     plt.figure()
-#                     plt.plot(fissa_out.raw[200, 20][0,:], label='Raw signal')
-#                     plt.plot(fissa_out.result[200, 20][0,:], label='result')
-#                     plt.plot(fissa_out.deltaf_raw[200, 20][0,:], label='dr raw')
-#                     plt.plot(fissa_out.deltaf_result[200, 20][0,:], label='df result')
-#                     plt.legend()
-#                     plt.show() """
-
-#                     print(p_num)
-#                     print(plane_path)
-#                     RawTwoData.Plane.insert1({**key, 'cell_select_done': 1, 'plane_n' : p_num, 'plane_path' : plane_path, 'data_df' : 'tmep'})
-
-#                     # TODO Add a confirmation about having selected cells
-#                     break
-
-#                     # Irrelevant now that we have FISSA
-#                     # f_data = np.load(os.path.join(plane_path, 'F.npy'))
-#                     # f_neu_data = np.load(os.path.join(plane_path, 'F.npy'))
-#                     # is_cell = np.load(os.path.join(plane_path, 'iscell.npy'))
-#                     #
-#                     # #filter for the selected cells
-#                     # cell_filt = is_cell[:, 0] == 0
-#                     # good_f = np.delete(f_data, cell_filt, axis=0)
-#                     # good_f_neu = np.delete(f_neu_data, cell_filt, axis=0)
--- datajoint_pipeline/tables/preprocessing_tables.py
+++ datajoint_pipeline/tables/preprocessing_tables.py
@@ -1,15 +1,15 @@
 import os
 import numpy as np
 from pathlib import Path
-from main_tables import Mouse, Session, Recording, DataPaths
+from datajoint_pipeline.tables.main_tables import Mouse, Session, Recording, DataPaths
 import datajoint as dj
-import helpers.nerfcluster_jobs as cluster_jobs
+import datajoint_pipeline.helpers.nerfcluster_jobs as cluster_jobs
 from datetime import datetime
 from natsort import natsorted
 import PySimpleGUI as sg
 # from suite2p.gui import gui2p
 import matplotlib.pyplot as plt
-from intermediaries.run_fissa_correction import run_fissa_correction
+from datajoint_pipeline.intermediaries.run_fissa_correction import run_fissa_correction
 from behavior_python.utils import parseStimpyLog
 import subprocess
 
@@ -113,7 +113,7 @@ class Preprocessing(dj.Computed):
 
 # TODO Find out it this can be made robust so that you don't need to add all the planes at once. - Maybe with a "cell_selection_done" field after all
 @schema
-class CellSelection(dj.Computed):
+class BasicRecordingData(dj.Computed):
     definition = """
     # The output from suite2p registration and ROI extraction
     -> Preprocessing
@@ -125,20 +125,9 @@ class CellSelection(dj.Computed):
     plane_frame_rate        : float             # The frame rate for each plane
     plane_frame_interval    : float             # The average time between frames for each plane
     flyback_frames          : int               # The number of flyback frames for the recording
-    cell_select_done        : int               # 0 if not yet complete, 1 otherwise
     """
 
-    class Plane(dj.Part):
-        definition = """
-        # The neuropil corrected dF/F for each plane of the recording
-        -> CellSelection
-        plane_n                 : int           # The number of the plane from the recording
-        ---
-        plane_path              : varchar(500)  # The path to the registered data for that specific plane
-        """
-    
     def make(self, key):
-
         # Get the major paths from the lookup table for the data we will need
         core_reg_path = os.path.join((DataPaths & 'data_type = "server_local"').fetch1('path_to_data'), r'2photon\reg')
         reg_data_path = os.path.join(core_reg_path, (Preprocessing & key).fetch1('reg_file_name'), 'suite2p')
@@ -162,7 +151,6 @@ class CellSelection(dj.Computed):
         plane_frame_interval = 1000 / plane_rate
 
         # Store the data about the recording for now.
-        # TODO Update cell_select_done after the cell selection is complete
         self.insert1({**key, 
             'n_planes' : num_of_planes,
             'frame_times': frame_times, 
@@ -170,73 +158,104 @@ class CellSelection(dj.Computed):
             'frame_rate' : frame_rate,
             'plane_frame_rate' : plane_rate,
             'plane_frame_interval' : plane_frame_interval,
-            'flyback_frames' : 0,
-            'cell_select_done' : 0,
+            'flyback_frames' : 0
             })
 
+@schema
+class Plane(dj.Imported):
+    definition = """
+    # The neuropil corrected dF/F for each plane of the recording
+    -> BasicRecordingData
+    plane_n                 : int           # The number of the plane from the recording
+    ---
+    plane_path              : varchar(500)  # The path to the registered data for that specific plane
+    plane_depth             : float         # the recording depth of the plane on the 2p microscope
+    """
+
+    def make(self, key):
+        
+        # Get the major paths from the lookup table for the data we will need
+        core_reg_path = os.path.join((DataPaths & 'data_type = "server_local"').fetch1('path_to_data'), r'2photon\reg')
+        reg_data_path = os.path.join(core_reg_path, (Preprocessing & key).fetch1('reg_file_name'), 'suite2p')
+        
+        # Get the paths to the suite2p output for each plane for this recording and find the number of planes
+        # TODO add sanity check that the number of planes in the suite2p folder matches the number of planes marked for the recording
+        plane_dirs = [f.name for f in os.scandir(reg_data_path) if f.is_dir() and 'plane' in f.name]
+        ordered_planes = natsorted(plane_dirs)
+
+        base_depth = (Recording & key).fetch1('depth')
+        #plane_depth_step =  (Recording & key).fetch1('plane_step')
         # Run through each plane for multi-plane recordings
+        # TODO add a skip for flybak frames in recordings
         for p in ordered_planes:
             p_num = int(p[5:])
 
-            """
-            sg.theme('DarkAmber')
-            layout = [[sg.Text('Run cell selection?')],
-                      [sg.Button('Ok'), sg.Button('No')] ]
-            window = sg.Window((Recording & key).fetch1('recording_name'), layout)
-
-            while True:
-                # TODO make this window more informative - session, plane num, etc
-                # TODO Add an option for if the window is closed and cell selection is incomplete
-                event, values = window.read()
-                if event == sg.WIN_CLOSED or event == 'No':
-                    break
-                elif event == 'Ok':
-                    window.close()
-            """
-            plane_path = os.path.join(reg_data_path, 'plane0')
-
-            stat_path = os.path.join(plane_path, 'stat.npy')
-            # TODO make sure that it stops if it lands on an error
-            result = subprocess.run(["python", r"C:\Users\dylan\Documents\Repositories\two_photon_pipeline_boninlab\intermediaries\run_suite2p_gui.py", "-i", stat_path], capture_output=True)
-            print(result)
-            print('Cell selection done')
-
-            print(p_num)
-            print(plane_path)
-            CellSelection.Plane.insert1({**key, 'plane_n' : p_num, 'plane_path' : plane_path})
-
-                    # TODO Add a confirmation about having selected cells
-                    
-            """
-            break
-            """
-
-                    # Irrelevant now that we have FISSA
-                    # f_data = np.load(os.path.join(plane_path, 'F.npy'))
-                    # f_neu_data = np.load(os.path.join(plane_path, 'F.npy'))
-                    # is_cell = np.load(os.path.join(plane_path, 'iscell.npy'))
-                    #
-                    # #filter for the selected cells
-                    # cell_filt = is_cell[:, 0] == 0
-                    # good_f = np.delete(f_data, cell_filt, axis=0)
-                    # good_f_neu = np.delete(f_neu_data, cell_filt, axis=0)
+            # TODO consider making this path relative
+            plane_path = os.path.join(reg_data_path, 'plane' + str(p_num))
+            plane_depth = 200
+            #Later, once we've updated the table
+            # plane_depth = base_depth + (plane_depth_step * p_num) # Should work because it's 0 indexed
+            self.insert1({**key, 'plane_n' : p_num, 'plane_path' : plane_path, 'plane_depth': plane_depth})
+        print(f'{p_num + 1} planes detected and logged for {(Recording & key).fetch1("recording_name")}')
+    
 
 
-class NeuropilCorrection(dj.Part):
+@schema
+class RoiSelection(dj.Computed):
+    definition = """
+    # Table to run cell selection for all frames 
+    -> Plane
+    ---
+    num_rois            : int               # The number of ROIs that were selected as cells during cell selection
+    """
+
+    def make(self, key):
+
+        stat_path = os.path.join((Plane & key).fetch1('plane_path'), 'stat.npy')
+        iscell_path = os.path.join((Plane & key).fetch1('plane_path'), 'iscell.npy')
+        # TODO make sure that it stops if it lands on an error
+        result = subprocess.run(["python", r"C:\Users\dylan\Documents\Repositories\two_photon_pipeline_boninlab\intermediaries\run_suite2p_gui.py", "-i", stat_path], capture_output=True)
+        print(result)
+
+        sg.theme('DarkAmber')
+        layout = [[sg.Text('Cell selection completed?')],
+                  [sg.Button('Yes'), sg.Button('No')] ]
+        window = sg.Window((Recording & key).fetch1('recording_name'), layout)
+
+        while True:
+            # TODO make this window more informative - session, plane num, etc
+            # TODO Add an option for if the window is closed and cell selection is incomplete
+            event, values = window.read()
+            if event == sg.WIN_CLOSED or event == 'No':
+                break
+            elif event == 'Yes':
+                window.close()
+                #'if event == 'Ok':
+                iscell = np.load(iscell_path)
+                n_rois = np.sum(iscell[:,0]==1)
+                self.insert1({**key, 'num_rois': n_rois})
+                break
+        
+
+@schema
+class NeuropilCorrection(dj.Computed):
     definition = """
         # The neuropil corrected dF/F for each plane of the recording
-        -> Plane
+        -> RoiSelection
         ---
-        data_df                 : longblob@nerffs     # The dF/F trace that is output from FISSA
+        data_df                 : longblob     # The dF/F trace that is output from FISSA
         """
+    # TODO get @nerffs to work
 
     def make(self, key):  
         
-        print(f"running neuropil correction for plane {(CellSelection.Plane & key).fetch1('plane_n')} of recording {(Preprocessing & key).fetch1('reg_file_name')}")
-        sampling_freq = (CellSelection & key).fetch1('plane_frame_interval')
+        print(f"running neuropil correction for plane {(Plane & key).fetch1('plane_n')} of recording {(Preprocessing & key).fetch1('reg_file_name')}")
+        sampling_freq = (BasicRecordingData & key).fetch1('plane_frame_interval')
         print(f"Sampling frequency: {sampling_freq}")
         
-        fissa_out = run_fissa_correction(plane_path, sampling_freq)
+        plane_path = (Plane & key).fetch1('plane_path')
+
+        fissa_out, rois = run_fissa_correction(plane_path, sampling_freq)
             #print(fissa_out)
         print('FISSA finished')
 
@@ -245,11 +264,10 @@ class NeuropilCorrection(dj.Part):
         num_frames = np.sum([np.shape(df_data[0, frames])[1] for frames in range(df_data.shape[1])])
         num_neurons = df_data.shape[0]
         df_cat = np.empty((num_neurons, num_frames))
-        for nn in num_neurons:
-            nn_trace = np.hstack([df_data[0, frames][0,:] for frames in range(df_data.shape(1))])
+        #print(df_cat.shape[:])
+        for nn in range(num_neurons):
+            nn_trace = np.hstack([df_data[nn, frames][0,:] for frames in range(df_data.shape[1])])
             df_cat[nn, :] = nn_trace
-        print('df output concatenated')
 
         # TODO add spike extraction
-
         self.insert1({**key, 'data_df' : df_cat})
