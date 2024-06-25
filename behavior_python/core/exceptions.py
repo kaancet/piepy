49733e68506d579ce48679706bb2e6e554e0c7ac
Kaan Cetindag <cetindag.kaan@gmail.com>
2024-06-25 14:59:36 +0200

    new exception type

--- behavior_python/core/exceptions.py
+++ behavior_python/core/exceptions.py
@@ -4,14 +4,18 @@ from behavior_python.utils import display
 class WrongSessionTypeError(BaseException):
     def __init__(self, *args: object) -> None:
         super().__init__(*args)
-        
-        
+
+
 class PrefProtMismatchError(BaseException):
     def __init__(self, *args: object) -> None:
         super().__init__(*args)
-        
-        
+
+
 class NoRigReactionTimeError(BaseException):
     def __init__(self, *args: object) -> None:
         super().__init__(*args)
-        
+
+
+class PathSettingError(BaseException):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
