14db8a7576d76f61bdf141c52debbb1f07604380
Kaan Cetindag <cetindag.kaan@gmail.com>
2024-11-23 14:44:40 +0100

    Added verbosity functionality to config

--- piepy/core/config.py
+++ piepy/core/config.py
@@ -13,5 +13,17 @@ class Config:
         for k, v in conf.items():
             setattr(self, k, v)
 
+    def _set(self, name: str, value) -> None:
+        """Function to set attributes"""
+        setattr(self, name, value)
+
+    ## some methods for on the fly changes to config file
+    def set_verbosity(self, val: bool) -> None:
+        """Set verbosity"""
+        if not isinstance(val, bool):
+            raise ValueError(f"Please provie a boolean to set verbosity, got {type(val)}")
+
+        self._set("verbose", val)
+
 
 config = Config()
