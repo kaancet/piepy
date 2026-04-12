3eba002e16e3d9a1153da81856bef9fff3d09761
Kaan Cetindag <cetindag.kaan@gmail.com>
2026-04-13 01:02:21 +0200

    organization of directories, slimmed down for pypi

--- piepy/core/config.py
+++ piepy/core/config.py
@@ -1,14 +1,30 @@
 import os
 import json
+from pathlib import Path
+
+# Prefilled JSON content
+CONFIG = {
+    "multiprocess": {"enable": False, "cores": 8},
+    "verbose": True,
+    "paths": {"colors": [str(Path.cwd().parents[0] / "plotters" / "colors")]},
+}
+
+PATHS = ["presentation", "training", "twop", "onepcam", "facecam", "eyecam", "opto_pattern", "analysis"]
 
 
 class Config:
     def __init__(self) -> None:
-        config_dir = os.path.dirname(
-            os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        )
-        with open(os.path.join(config_dir, "config.json")) as json_file:
-            conf = json.load(json_file)
+
+        self.home_dir = Path.home()
+        self.config_dir = self.home_dir / ".piepy"
+        self.file_path = self.config_dir / "config.json"
+
+        # Check if file already exists
+        if self.file_path.exists():
+            with open(self.file_path) as json_file:
+                conf = json.load(json_file)
+        else:
+            conf = self.make_config()
 
         for k, v in conf.items():
             setattr(self, k, v)
@@ -21,11 +37,25 @@ class Config:
     def set_verbosity(self, val: bool) -> None:
         """Set verbosity"""
         if not isinstance(val, bool):
-            raise ValueError(
-                f"Please provie a boolean to set verbosity, got {type(val)}"
-            )
+            raise ValueError(f"Please provie a boolean to set verbosity, got {type(val)}")
 
         self._set("verbose", val)
+
+    def make_config(self):
+        """Creates a .piepy folder at the home directory and puts config.json in there"""
+        print(f"Creating .piepy directory at {self.home_dir}", flush=True)
+        os.makedirs(self.config_dir, exist_ok=True)
+
+        # add the paths variable
+        config = CONFIG.copy()
+
+        for p in PATHS:
+            config["paths"][p] = [f"data/{p}"]
+
+        with self.file_path.open("w", encoding="utf-8") as f:
+            json.dump(config, f, indent=4)
+
+        return config
 
 
 config = Config()
